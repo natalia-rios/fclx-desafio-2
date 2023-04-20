69a30fb5a70a1271e2c17e0e245e4303e9623448
riosnatalia <natalia.rios@wexinc.com>
2023-04-20 17:30:12 -0300

    fix: fix route bug

--- src/app/prisma/prisma.ts
+++ src/app/prisma/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-
 const globalForPrisma = global as { prisma?: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
